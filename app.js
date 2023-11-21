a1f6b29706554d6fdda3790cc43c8906c6c0063e
MiguelRen <migueleduardorengel@gmail.com>
2023-11-21 16:17:53 -0400

    trying to get the error in app.js

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ app.use((req,res, next) => {
 });
 
 app.use((error,req,res,next) => {
-    console.log("error2");
+    console.log(error);
+    console.log(req);
     res.status(error.status || 555);
     res.json({
         error:{
