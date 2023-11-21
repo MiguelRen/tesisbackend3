73c30c3c19a678d4f15eb56e452fb03deb04c7d0
MiguelRen <migueleduardorengel@gmail.com>
2023-11-21 16:01:59 -0400

    adding logs into app.js

--- app.js
+++ app.js
@@ -58,18 +58,19 @@ app.use('/api/quarter',quarterRoutes);
 
 // error handling
 app.use((req,res, next) => {
+    console.log("error1");
     const error = newError('Not Found');
-    
     error.status= 404;
     next(error);
 
 });
 
 app.use((error,req,res,next) => {
+    console.log("error2");
     res.status(error.status || 500);
     res.json({
         error:{
-            
+            message:"error2 inside -res-",
             message: error.message
         }
     })
