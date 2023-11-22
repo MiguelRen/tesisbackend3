56b77c17b333861a77994e6896fef33b2ee58721
MiguelRen <migueleduardorengel@gmail.com>
2023-11-22 09:46:17 -0400

    taking off  pg-connection-string

--- api/database/database.js
+++ api/database/database.js
@@ -1,9 +1,9 @@
 import pg from "pg";
 import config from "./config.js";
 
-import parse from pg-connection-string.parse
+// import parse from pg-connection-string.parse
 
-const connectionString =parse('postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass');
+// const connectionString =parse('postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass');
 // console.table(pg)
 const {Pool} = pg;
 const pool = new Pool({
@@ -11,8 +11,8 @@ const pool = new Pool({
     // database : config.database,
     // user : config.user,
     // password : config.password,
-    connectionString,
-    // database_url : config.database_url,
+    // connectionString,
+    database_url : config.database_url,
     ssl: {
         rejectUnauthorized:false,
     },
