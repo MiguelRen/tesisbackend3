70089d46f037bf0f07cf679425229947fed5780f
MiguelRen <migueleduardorengel@gmail.com>
2023-11-22 16:45:18 -0400

    adding .env POSTGRES_URL

--- api/database/config.js
+++ api/database/config.js
@@ -7,7 +7,7 @@ export default {
     // host :process.env.HOST ||  "localhost" ,
     // database :  process.env.DATABASE || "simpleclass" ,
     // user :   process.env.USER || "miguel" ,
-    // password : process.env.PASSWORD || "456123" ,
+    password : process.env.PASSWORD || "456123" ,
     // port : process.env.PORT || "5432",
     database_url : process.env.DATABASE_URL,
     
--- api/database/database.js
+++ api/database/database.js
@@ -7,17 +7,23 @@ import config from "./config.js";
 // console.table(pg)
 const {Pool} = pg;
 const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
     // host : config.host,
     // database : config.database,
     // user : config.user,
-    // password : config.password,
+    // password : configk.password,
     // connectionString,
     // database_url : config.database_url,
     // database_url:"postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass",
-    database_url:"postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a.oregon-postgres.render.com/simpleclass",
-    ssl: {
-        rejectUnauthorized:false,
-    },
+    // database_url:"postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a.oregon-postgres.render.com/simpleclass",
+    
+    // ssl: false,
+    // {
+    //     rejectUnauthorized:false,
+     
+    // },
+    // password : "456123",
+    // database:"simpleclass",
 
 });
 
