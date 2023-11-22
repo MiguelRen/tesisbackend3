a8fa4fa7bde62bfbf77fa046bd6ba680416ea6e1
MiguelRen <migueleduardorengel@gmail.com>
2023-11-21 22:38:53 -0400

    connection string x2

--- api/database/database.js
+++ api/database/database.js
@@ -1,6 +1,9 @@
 import pg from "pg";
 import config from "./config.js";
-const connectionString = "postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass";
+
+import parse from pg-connection-string.parse
+
+const connectionString =parse('postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass');
 // console.table(pg)
 const {Pool} = pg;
 const pool = new Pool({
