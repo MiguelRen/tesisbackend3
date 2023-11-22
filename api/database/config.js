ed90f997064f8e1e45b04dde42dd37f09c6694ec
MiguelRen <migueleduardorengel@gmail.com>
2023-11-21 22:01:32 -0400

    I dont kno what I write

--- api/database/config.js
+++ api/database/config.js
@@ -2,11 +2,11 @@ import {config} from 'dotenv';
 config();
 
 export default {
-    host :process.env.HOST ||  "localhost" ,
-    database :  process.env.DATABASE || "simpleclass" ,
-    user :   process.env.USER || "miguel" ,
-    password : process.env.PASSWORD || "456123" ,
-    port : process.env.PORT || "5432",
+    // host :process.env.HOST ||  "localhost" ,
+    // database :  process.env.DATABASE || "simpleclass" ,
+    // user :   process.env.USER || "miguel" ,
+    // password : process.env.PASSWORD || "456123" ,
+    // port : process.env.PORT || "5432",
     database_url : process.env.DATABASE_URL,
     
 
