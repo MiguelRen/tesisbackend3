4f86cbfe72f74b8ed2595dbe25d9593e925c3625
MiguelRen <migueleduardorengel@gmail.com>
2024-03-13 15:20:24 -0400

    changing config.js (database)

--- api/database/config.js
+++ api/database/config.js
@@ -4,10 +4,10 @@ config();
 // connectionString
 
 export default {
-  host: process.env.HOST || "dpg-cnl9092cn0vc73fiug50-a/simpleclass_ptug",
-  database: process.env.DATABASE || "simpleclass_ptug",
-  user: process.env.USER || "miguel",
-  port: process.env.PORT || "5432",
-  password: process.env.PASSWORD || "456123",
+  host: "dpg-cnl9092cn0vc73fiug50-a",
+  database:"simpleclass_ptug",
+  user: "miguel",
+  port:"5432",
+  password: "O2dc69APkpmhid3sK0o4Ww9tsX7yRHJSgit",
   database_url : "postgres://miguel:O2dc69APkpmhid3sK0o4Ww9tsX7yRHJS@dpg-cnl9092cn0vc73fiug50-a/simpleclass_ptug",
 };
