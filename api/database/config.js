3732d4196c4d1d3b9887577564c5c37dbb1e3f3e
MiguelRen <migueleduardorengel@gmail.com>
2024-03-13 15:03:10 -0400

    changing .env by finall server var

--- api/database/config.js
+++ api/database/config.js
@@ -4,10 +4,10 @@ config();
 // connectionString
 
 export default {
-  host: process.env.HOST || "localhost",
-  database: process.env.DATABASE || "simpleclass",
+  host: process.env.HOST || "dpg-cnl9092cn0vc73fiug50-a/simpleclass_ptug",
+  database: process.env.DATABASE || "simpleclass_ptug",
   user: process.env.USER || "miguel",
   port: process.env.PORT || "5432",
   password: process.env.PASSWORD || "456123",
-  // database_url : process.env.DATABASE_URL,
+  database_url : "postgres://miguel:O2dc69APkpmhid3sK0o4Ww9tsX7yRHJS@dpg-cnl9092cn0vc73fiug50-a/simpleclass_ptug",
 };
