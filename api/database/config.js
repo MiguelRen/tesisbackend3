d97e60fecf88473f1aa7d972a48934de6898774b
MiguelRen <migueleduardorengel@gmail.com>
2024-03-13 15:50:57 -0400

    password changing

--- api/database/config.js
+++ api/database/config.js
@@ -8,6 +8,6 @@ export default {
   database:"simpleclass_ptug",
   user: "miguel",
   port:"5432",
-  password: "O2dc69APkpmhid3sK0o4Ww9tsX7yRHJSgit",
+  password: "O2dc69APkpmhid3sK0o4Ww9tsX7yRHJS",
   database_url : "postgres://miguel:O2dc69APkpmhid3sK0o4Ww9tsX7yRHJS@dpg-cnl9092cn0vc73fiug50-a/simpleclass_ptug",
 };
