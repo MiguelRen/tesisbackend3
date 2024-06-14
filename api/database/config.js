ba2fcb8d598699a24a530a860fb798af3429b94e
MiguelRen <migueleduardorengel@gmail.com>
2024-06-14 10:31:33 -0400

    another database change

--- api/database/config.js
+++ api/database/config.js
@@ -5,9 +5,9 @@ config();
 
 export default {
   host: "dpg-cphie98cmk4c73eihtq0-a/simpleclass_11ml",
-  database:"simpleclass_11ml",
-  user: "miguel",
+  database:"simpleclass_1x4s",
+  user: "simpleclass_1x4s_user",
   port:"5432",
-  password: "o3CBL0pxeGZ8FkG8s8w2nwuy6kQt7i68S",
-  database_url : "postgres://miguel:o3CBL0pxeGZ8FkG8s8w2nwuy6kQt7i68@dpg-cphie98cmk4c73eihtq0-a/simpleclass_11ml",
+  password: "dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
+  database_url : "postgres://simpleclass_1x4s_user:qZrPd4Z7f5fMULyFQPT52qGEI3SEkFoz@dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
 };
