8c7b7cd11be1459ef85be90426fc3d372a4c50d7
MiguelRen <migueleduardorengel@gmail.com>
2024-06-16 05:20:23 -0400

    database configurations

--- api/database/config.js
+++ api/database/config.js
@@ -4,10 +4,10 @@ config();
 // connectionString
 
 export default {
-  host: "dpg-cphie98cmk4c73eihtq0-a/simpleclass_11ml",
+  host: "dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
   database:"simpleclass_1x4s",
   user: "simpleclass_1x4s_user",
   port:"5432",
-  password: "dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
-  database_url : "postgres://simpleclass_1x4s_user:qZrPd4Z7f5fMULyFQPT52qGEI3SEkFoz@dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
+  password: "qZrPd4Z7f5fMULyFQPT52qGEI3SEkFoz",
+  database_url : "dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
 };
