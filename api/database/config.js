fd1ab5b1bca6ae7d1ff58ec9201c100c728a942f
MiguelRen <migueleduardorengel@gmail.com>
2024-06-18 05:03:14 -0400

    .env changes

--- api/database/config.js
+++ api/database/config.js
@@ -4,10 +4,9 @@ config();
 // connectionString
 
 export default {
-  host: "dpg-cpm540eehbks73d8s5gg-a",
-  database:"simpleclass_1x4s",
-  user: "simpleclass_1x4s_user",
-  port:"5432",
-  password: "qZrPd4Z7f5fMULyFQPT52qGEI3SEkFoz",
-  database_url : "dpg-cpm540eehbks73d8s5gg-a/simpleclass_1x4s",
+  host: process.env.HOST || "localhost",
+  database: process.env.DATABASE || "simpleclass",
+  user: process.env.USER || "miguel",
+  port: process.env.PORT || "5432",
+  password: process.env.PASSWORD || "456123",
 };
