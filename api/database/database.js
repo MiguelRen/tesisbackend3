2c6eace8ffce88fa539dad677bda602883abd55b
MiguelRen <migueleduardorengel@gmail.com>
2024-06-18 05:47:30 -0400

    dataase chaqnges

--- api/database/database.js
+++ api/database/database.js
@@ -1,10 +1,7 @@
 import pg from "pg";
 import config from "./config.js";
 
-// import parse from pg-connection-string.parse
 
-// const connectionString =parse('postgres://miguel:vhmKQup0dtQL0zuNmyaFl1mtT7oBHwGO@dpg-cld8prmg1b2c73f55i5g-a/simpleclass');
-// console.table(pg)
 const { Pool } = pg;
 const pool = new Pool({
   // connectionString: process.env.POSTGRES_URL,
